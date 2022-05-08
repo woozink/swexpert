337c17f124c470f5ae1509a1a8ba58a07885d405
woozink <k0109974@naver.com>
2022-05-08 14:57:40 +0900

    1

---
+++ src/d1/Stamp.java
@@ -0,0 +1,19 @@
+package d1;
+
+import java.util.*;
+import java.io.*;
+
+public class Stamp {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < n; i++) {
+			System.out.print("*");
+		}
+		
+		
+		
+	}
+}
