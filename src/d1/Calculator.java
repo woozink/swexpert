f4ae16c6716acd6b7a0a52bbf805ffec7b24cb17
woozink <k0109974@naver.com>
2022-05-10 19:04:23 +0900

    1

--- src/d1/Calculator.java
+++ src/d1/Calculator.java
@@ -1,5 +1,19 @@
 package d1;
+import java.io.*;
+import java.util.*;
 
 public class Calculator {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
 
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+
+	}
 }
