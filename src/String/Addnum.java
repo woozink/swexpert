869654ff19e2fb1ffd5921527927c8dce1aed7b3
woozink <k0109974@naver.com>
2022-05-23 14:46:04 +0900

    saffy String 3

--- src/String/Addnum.java
+++ src/String/Addnum.java
@@ -10,7 +10,7 @@ public class Addnum {
 		String a = br.readLine();
 		int sum = 0;
 		for(int i = 0; i < n; i ++) {
-			sum += a.charAt(i)-'45';
+			sum += a.charAt(i)-'0';
 		}
 		
 		System.out.println(sum);
