f06ddd0a480ad8d082d4825dc504e4441dbc77e7
woozink <k0109974@naver.com>
2022-05-24 13:19:52 +0900

    2

---
+++ src/String/AgainstStr.java
@@ -0,0 +1,26 @@
+package String;
+
+import java.io.*;
+import java.util.*;
+
+public class AgainstStr {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+
+		for (int tc = 0; tc < T; tc++) {
+
+			String[] str = br.readLine().split(" "); // 공백 분리
+			int R = Integer.parseInt(str[0]); // String -> int
+			String S = str[1];
+
+			for (int i = 0; i < S.length(); i++) {
+				for (int j = 0; j < R; j++) {
+					System.out.print(S.charAt(i));
+				}
+			}
+			System.out.println();
+
+		}
+	}
+}
--- src/kakao2022/DecrationResult.java
+++ src/kakao2022/DecrationResult.java
@@ -1,5 +1,21 @@
 package kakao2022;
 
-public class DecrationResult {
+import java.util.*;
+import java.io.*;
 
-}
+public class DecrationResult {
+	public int [] solution(String[] id_list, String[] report, int k ) {
+		List<String> list = Arrays.stream(report).distinct().collect(Collectors.tolist());
+		HashMap<Stirng, Integer> count = new HashMap<>();
+		
+		for(String s : list) {
+			String target = s.split(" ")[1];
+			count.put(target,count.getOrDefault(target,0)+1);
+		}
+		
+		 return Arrays.stream(id_list).map(_user -> {
+	            final String user = _user;
+	            List<String> reportList = list.stream().filter(s -> s.startsWith(user + " ")).collect(Collectors.toList());
+	            return reportList.stream().filter(s -> count.getOrDefault(s.split(" ")[1], 0) >= k).count();
+	        }).mapToInt(Long::intValue).toArray();
+	    }
